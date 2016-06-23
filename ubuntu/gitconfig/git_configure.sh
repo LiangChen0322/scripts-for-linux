bcfb928e27bcf79c15b71ffe3c37fb555903f2ce
Chen Liang <liangchen0322@gmail.com>
2016-06-23 21:04:11 +0800

    Add basic git configure for Ubuntu
    Signed-off-by: Chen Liang <liangchen0322@gmail.com>

---
+++ ubuntu/gitconfig/git_configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# git config --global user.name [username]
+# git config --global user.email [email]
+
+# git config --global color.ui true
+# git config --global core.editor vim
+
+# sudo git config --system alias.st status    #git st
+# sudo git config --system alias.ci commit    #git commit
+# sudo git config --system alias.co checkout  #git co
+# sudo git config --system alias.br  branch   #git branch
+
+mv ./gitconfig ~/.gitconfig
