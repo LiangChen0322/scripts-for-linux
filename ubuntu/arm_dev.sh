0ad911e8cbd795a666d50a88448cb425e77e4390
Chen Liang <liangchen0322@gmail.com>
2016-06-26 11:53:54 +0800

    Add arm basic dev environment building
    Signed-off-by: Chen Liang <liangchen0322@gmail.com>

---
+++ ubuntu/arm_dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install gcc-arm-linux-gnueabi -y
+sudo apt-get install gcc-arm-linux-gnueabihf -y
+
+sudo apt-get install g++-arm-linux-gnueabi -y
+sudo apt-get install g++-arm-linux-gnueabihf -y
